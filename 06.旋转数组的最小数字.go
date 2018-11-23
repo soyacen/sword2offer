f639edaa481aa6dd918388561a8f11704ad99c7a
yacen <434432458@qq.com>
2018-11-23 17:43:33 +0800

    旋转数组最小值

--- 06.旋转数组的最小数字.go
+++ 06.旋转数组的最小数字.go
@@ -7,10 +7,11 @@ import "fmt"
 // 例如数组{3,4,5,1,2}为{1,2,3,4,5}的一个旋转，该数组的最小值为1。
 // NOTE：给出的所有元素都大于0，若数组大小为0，请返回0。
 
+// 1。循环，2,最大值后面的，3。分治
+
 func main() {
 	array := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 	rotateArray := rotateArray(array, 7) //[]int{3,4,5,1,2}
-	fmt.Println(rotateArray)
 	fmt.Println(minNumberInRotateArray(rotateArray))
 }
 
@@ -20,29 +21,27 @@ func rotateArray(array []int, n int) (result []int) {
 	return
 }
 
-func minNumberInRotateArray(array []int) int {
+func minNumberInRotateArray(array []int) (min int) {
 	if len(array) == 0 {
-		return 0
+		return
 	}
 	left := 0
 	right := len(array) - 1
-	middle := 0
-	for left <= right {
-		middle = (left + right) / 2
-		if array[middle] > array[left] {
-			left = middle + 1
-			break
-		} else {
-			right = middle
-			break
-		}
-		if array[middle] > array[right] {
-			left = middle + 1
-			break
-		} else {
-			right = middle
-			break
-		}
+	if array[left] <= array[right] {
+		return array[0]
 	}
-	return array[middle]
+	if len(array) == 2 && array[left] > array[right] {
+		return array[1]
+	}
+	middle := (left + right) / 2
+	minLeft := minNumberInRotateArray(array[:middle])
+	minRight := minNumberInRotateArray(array[middle+1:])
+	min = array[middle]
+	if minLeft != 0 && minLeft < min {
+		min = minLeft
+	}
+	if minRight != 0 && minRight < min {
+		min = minRight
+	}
+	return
 }
